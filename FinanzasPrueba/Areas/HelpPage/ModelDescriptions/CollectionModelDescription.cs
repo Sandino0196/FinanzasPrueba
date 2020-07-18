44b4d72ada5f2163a588dedb3a7931946eec3207
Sandino <sandinorodrigo@gmail.com>
2020-07-17 18:53:16 -0600

    Agregar archivos de proyecto.

---
+++ FinanzasPrueba/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FinanzasPrueba.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FinanzasPrueba/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FinanzasPrueba.Startup))]
+
+namespace FinanzasPrueba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
