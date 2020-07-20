0f37c1d64567aaf8005d058d4c4953c367967253
Sandino <sandinorodrigo@gmail.com>
2020-07-20 10:47:21 -0600

    Registro

--- FinanzasPrueba/Controllers/AccountController.cs
+++ FinanzasPrueba/Controllers/AccountController.cs
@@ -330,7 +330,7 @@ namespace FinanzasPrueba.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
